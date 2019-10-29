61a07d82a15a52ff3c16d50a95bc8f0f34bc32db
Naty4393 <naty4393@gmail.com>
2019-10-29 10:03:31 -0500

    actualización

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { example } from './data.js'
 const name = document.getElementById('name')
 
 name.innerHTML = Object.values(LoL.data.Aatrox)
-
+//hola mundo
 console.assert(LoL)
 console.log(example);
 

e7760e5fd97cfcdb7b0479785ff47b6e709d03fe
Alelizsanchez <Alelizsanchez@gmail.com>
2019-11-04 10:55:08 -0500

    actualiza los cambios de css

--- src/data.js
+++ src/data.js
@@ -2,7 +2,20 @@
 
 // esta es una función de ejemplo
 
-export const example = () => {
-  return 'example';
+// export const example = () => {
+//   return 'example';
+// };
+export const mostrarCampiones = (data) => {
+  console.log(data);
+  let almacenar = '';
+  // eslint-disable-next-line array-callback-return
+  data.map((extraer) => {
+    almacenar += `
+    ${extraer.splash}
+    ${extraer.name}
+    `;
+  });
+  return console.log(almacenar);
 };
 
+// export default mostrarCampiones;
--- src/main.js
+++ src/main.js
@@ -1,34 +1,6 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import POTTER from './data/potter/potter.js' */
-import LoL from './data/lol/lol.js'
+import LoL from './data/lol/lol.js';
+// eslint-disable-next-line import/named
+import { mostrarCampiones } from './data';
 
-
-const name = document.getElementById('name')
-
-// import lol from './data/lol/lol.js';
-
-console.log(example);
-
-/*
- * console.log(POKEMON);
- * console.log(POTTER);*/
-
-console.log(LoL)
-let lolData = Object.values(LoL.data);
-//  const lolData = LoL.data;
- const $root = document.querySelector('#root');
-
-const playing = (data) => {
-    let almacenar = '';
-    for (let i = 0; i < data.length; i++) {
-    let item = `<div class="contenedor flex" >
-      <img class="img__lol" src="${data[i].splash}"/>
-      <p class ="name"> ${data[i].name}</p>
-      </div>`;
-        almacenar += item;
-    }
-    return almacenar;
-};
-
-$root.innerHTML = playing(lolData);
+const lolData = Object.values(LoL.data);
+document.querySelector('#root').innerHTML = mostrarCampiones(lolData);
